43df14a6ec31d72d55c20ab2c6f81d065a1125f3
Gordon Gill <gmilly@Gordons-MacBook-Air.local>
2024-12-08 17:22:15 +1100

    Cleaned up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from './components/Home';
+import Home from './components/sections/Home';
 import { themeStyles } from "./styles/theme";
 
 
---
+++ src/components/sections/Home.js
@@ -0,0 +1,38 @@
+import HeroSection from "./HeroSection";
+import AboutSection from "./AboutSection";
+import SkillsSection from "./SkillsSection";
+import ProjectsSection from "./ProjectsSection";
+import ContactSection from "./ContactSection";
+
+const Home = ({ theme, toggleTheme }) => {
+  return (
+    <div
+      style={{
+        width: "100vw",
+        minHeight: "100vh",
+        background: theme.background, // Apply the theme's background
+        color: theme.textColor, // Apply the theme's text color
+        fontFamily: "'Poppins', sans-serif",
+        transition: "background 0.9s, color 0.9s", // Smooth transition for theme changes
+      }}
+    >
+      <HeroSection theme={theme} toggleTheme={toggleTheme} />
+            {/* Gradient Line Divider */}
+            <div
+        style={{
+          width: "100%",
+          height: "10px",
+          background: "linear-gradient(90deg, #3c82f6, #9147ff)",
+          margin: "0 auto",
+          position: "relative",
+        }}
+      ></div>
+      <AboutSection theme={theme} />
+      <SkillsSection theme={theme} />
+      <ProjectsSection theme={theme} />
+      <ContactSection theme={theme} />
+    </div>
+  );
+};
+
+export default Home;
